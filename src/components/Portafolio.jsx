b8ddf5cfb85b2038408858d25978050500f09c61
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-02-28 22:19:31 -0600

    finishing portafolio component

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,18 +1,46 @@
 import React from "react";
 import styles from "../styles/portafolio.css";
+import { SiGithub } from "react-icons/si";
+import { SlGlobe } from "react-icons/sl";
+import data from "../data.js";
 
 const Portafolio = () => {
   return (
     <section id="portafolio">
       <h5>My recent work</h5>
-      <h2>
-        Portafolio
-      </h2>
+      <h2>Portfolio</h2>
 
-      <div
-      className="container portafolio__container"
-      >
-
+      <div className="container portafolio__container">
+        {data.map(({ id, title, image, github, live }) => {
+          return (
+            <article key={id} className=" portafolio__item ">
+              <div className="portafolio__item__img">
+                <img src={image} alt="web" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portafolio__item__text">
+                <div className="">
+                  <a
+                    href={github}
+                    target="_blank"
+                    className="btn btn-portafolio"
+                  >
+                    {<SiGithub />} GitHub
+                  </a>
+                </div>
+                <div className="">
+                  <a
+                    href={live}
+                    target="_blank"
+                    className="btn btn-primary btn-portafolio"
+                  >
+                    {<SlGlobe />} Live Demo
+                  </a>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/data.js
@@ -0,0 +1,48 @@
+import web from "./assets/web2.jpg";
+
+const data = [
+    {
+        id: 1,
+        image: web,
+        title: "The best of the best",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+    {
+        id: 2,
+        image: web,
+        title: "The best of the best2",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+    {
+        id: 3,
+        image: web,
+        title: "The best of the best",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+    {
+        id: 4,
+        image: web,
+        title: "The best of the best",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+    {
+        id: 5,
+        image: web,
+        title: "The best of the best",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+    {
+        id: 6,
+        image: web,
+        title: "The best of the best",
+        github: "www.github.com",
+        demo: "www.demo.com",
+    },
+];
+
+export default data;
