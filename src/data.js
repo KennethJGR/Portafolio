a576e4918acf79c5e088348371dafb8185f3f18f
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-06 17:40:04 -0600

    finishing v1.0

--- src/data.js
+++ src/data.js
@@ -1,4 +1,9 @@
 import portfolio from "./assets/Portafolio.png";
+import vet from "./assets/Vetpatient.png";
+import Coin from "./assets/Crypto.png";
+import expense from "./assets/Expense.png";
+import Coin21 from "./assets/Crypto.png";
+import Coin23 from "./assets/Crypto.png";
 
 const data = [
     {
@@ -10,39 +15,26 @@ const data = [
     },
     {
         id: 2,
-        image: portfolio,
-        title: "My Portfolio",
-        github: "https://github.com/KennethJGR/Portafolio",
-        demo: "https://kennethgaitanportafolio.netlify.app/",
+        image: expense,
+        title: "Expense Planner",
+        github: "https://github.com/KennethJGR/Cost-Control",
+        demo: "https://expenseplannerk.netlify.app/",
     },
     {
         id: 3,
-        image: portfolio,
-        title: "My Portfolio",
-        github: "https://github.com/KennethJGR/Portafolio",
-        demo: "https://kennethgaitanportafolio.netlify.app/",
+        image: vet,
+        title: "Veterinary patient manager",
+        github: "https://github.com/KennethJGR/citas-React",
+        demo: "https://vetreactdates.netlify.app/",
     },
     {
         id: 4,
-        image: portfolio,
-        title: "My Portfolio",
-        github: "https://github.com/KennethJGR/Portafolio",
-        demo: "https://kennethgaitanportafolio.netlify.app/",
+        image: Coin,
+        title: "Cryptocurrency app",
+        github: "https://github.com/KennethJGR/crypto-react",
+        demo: "https://cryptocurrencyken.netlify.app/",
     },
-    {
-        id: 5,
-        image: portfolio,
-        title: "My Portfolio",
-        github: "https://github.com/KennethJGR/Portafolio",
-        demo: "https://kennethgaitanportafolio.netlify.app/",
-    },
-    {
-        id: 6,
-        image: portfolio,
-        title: "My Portfolio",
-        github: "https://github.com/KennethJGR/Portafolio",
-        demo: "https://kennethgaitanportafolio.netlify.app/",
-    },
+
 ];
 
 export default data;
